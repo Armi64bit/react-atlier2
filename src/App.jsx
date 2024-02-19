0448a62f6516127d184e5d8f527e0ca9f08560ac
Armi64bit <bahaeddine.bouzid@esprit.tn>
2024-02-19 11:00:23 +0100

    nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Events from "./components/Events";
+import About from "./components/About";
+import N404 from "./components/NotFound";
+
 function App() {
   
   return (
-    <Events></Events>
+   
+    <Routes>
+      <Route path="/events" element={<Events />} />
+      <Route path="/about/:username" element={<About />} />
+      
+      <Route path="" element={<N404/>} />
+    </Routes>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { BrowserRouter } from 'react-router-dom';
+import { NavigationBar } from './components/NavigationBar.jsx';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter>
+        <NavigationBar />
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
     <App />
-     
-  </React.StrictMode>,
-)
+  </BrowserRouter>
+);
+
